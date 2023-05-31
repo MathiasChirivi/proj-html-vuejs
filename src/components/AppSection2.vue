a4ece143dda33e42ebaa0b74c95fa0abffe01518
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-31 11:37:06 +0200

    section 2 finish

--- src/components/AppSection2.vue
+++ src/components/AppSection2.vue
@@ -7,7 +7,7 @@ export default {
 <template>
     <div class="headerBg ">
         <div class="container containerMain d-flex flex-column align-items-center">
-            <img class="imgClock mb-4" src="../assets/img/Clock-and-Bell.png" alt="">
+            <img class="imgClockCalendar mb-4" src="../assets/img/Clock-and-Bell.png" alt="">
             <h2 class="bg-danger mt-4 mb-5 text-white">University Year</h2>
             <div class="rowFirst text-center text-white">
                 <div class="text">
@@ -34,6 +34,60 @@ export default {
                     </p>
                 </div>
             </div>
+            <img class="imgClockCalendar calendarImg" src="../assets/img/upcoming-events-calendar-icon.png" alt=""
+                srcset="">
+            <h2 class="text-white mb-5">Upcoming Events</h2>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h3 class="card-title text-center">Coaching Session</h3>
+                            <div class="d-flex justify-content-center">
+                                <font-awesome-icon :icon="['fas', 'calendar-days']" />
+                                <p class="m-0 mx-2">20 May 21:30 PM</p>
+                            </div>
+                            <p class="card-text text-center lh-sm">This is a longer card with supporting text below as a
+                                natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btnFind  text-center">Find More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col position-relative">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h3 class="card-title text-center">Coaching Session</h3>
+                            <div class="d-flex align-items-center justify-content-center">
+                                <font-awesome-icon :icon="['fas', 'calendar-days']" />
+                                <p class="m-0 mx-2">20 May 21:30 PM</p>
+                            </div>
+                            <p class="card-text text-center lh-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed
+                                do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <a href="#" class="btnFind  text-center">Find More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card heightBody">
+                        <div class="card-body">
+                            <h3 class="card-title text-center">Coaching Session</h3>
+                            <div class="d-flex justify-content-center">
+                                <font-awesome-icon :icon="['fas', 'calendar-days']" />
+                                <p class="m-0 mx-2">20 May 21:30 PM</p>
+                            </div>
+                            <p class="card-text text-center lh-sm">This is a longer card with supporting text below as a
+                                natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btnFind  text-center">Find More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="buttonMargin p-3">View All Events</button>
         </div>
     </div>
 </template>
@@ -48,8 +102,13 @@ export default {
     margin: 0 auto;
 }
 
-.imgClock {
+.imgClockCalendar {
     width: 5%;
+}
+
+.calendarImg {
+    margin-top: 100px;
+    margin-bottom: 50px;
 }
 
 .timeLineItem {
@@ -90,5 +149,36 @@ export default {
 .rowLast .text:last-child {
     margin-left: 130px;
     margin-right: 260px;
+}
+
+a {
+    width: 180px;
+    position: absolute;
+    bottom: -30px;
+    left: 50%;
+    transform: translateX(-50%);
+    text-decoration: none;
+}
+
+.btnFind {
+    padding: 15px;
+    font-size: 14px;
+    border-radius: 35px;
+    color: #FFFFFF;
+    background-color: #E56768;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 20px 0px;
+}
+
+.heightBody {
+    height: 180px;
+}
+
+.buttonMargin {
+    width: 170px;
+    margin-top: 130px;
+    border: #E56768;
+    background-color: #E9D758;
+    color: white;
+    border-radius: 30px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).mount('#app')
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+library.add(faCalendarDays)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
