0bd1909f030297fe82bcd45f775180f78ca27bbc
MathiasChirivi <chirivimathias3@gmail.com>
2023-06-06 10:54:31 +0200

    add blackbox in section0

--- src/components/AppSection0.vue
+++ src/components/AppSection0.vue
@@ -6,8 +6,14 @@ export default {
 
 <!-- In this section, we start with the Main with various text and image -->
 <template>
+    <div class="mx-5 blackBox position-relative ">
+        <p class="mb-0">Purchase EduPrime On Envato Market</p>
+        <font-awesome-icon class="CaretDown position-absolute" :icon="['fas', 'caret-down']" />
+    </div>
+
     <div class="container containerMain d-flex">
         <div class="left d-flex flex-column justify-content-center">
+            <img src="../assets/img/Books-icon.png" alt="">
             <h2>Thousands of courses for any type of Student</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque atque temporibus sit. Voluptates minus
                 cupiditate nemo facilis debitis ea, doloremque ut, saepe autem quaerat, eum consequuntur sit aperiam
@@ -42,6 +48,24 @@ export default {
 .right {
     width: calc(100% / 2);
     height: 500px;
+}
+
+.left img {
+    width: 10%;
+    margin-bottom: 20px;
+}
+
+.blackBox {
+    width: 250px;
+    text-align: center;
+    border: 1px solid black;
+    color: white;
+    background-color: #222;
+}
+
+.CaretDown {
+    color: #222;
+    bottom: -10px;
 }
 
 .right img {
--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
@@ -27,6 +28,7 @@ library.add(faBookOpen)
 library.add(faCircleInfo)
 library.add(faCartShopping)
 library.add(faChevronUp)
+library.add(faCaretDown)
 library.add(faUserPlus)
 library.add(faMagnifyingGlass)
 library.add(faCalendarDays)
